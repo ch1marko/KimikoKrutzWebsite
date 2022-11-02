64c79c5084da31b638bb080af20e60d45952c841
Marko <marko.dzomba.info@gmail.com>
2022-11-02 15:58:38 +0100

    finishing touches

--- script.js
+++ script.js
@@ -93,22 +93,23 @@ widthChangeCallback(myMediaQuery);
 
 
 // Close mobile menu on click outside
-document.addEventListener( 'click', function( evt ) {
-    var openedMenu = openedMenu = document.querySelector( '.navbar-toggler .navbar-toggler-icon' ),
-        targetElement = evt.target;
+/*document.addEventListener( 'click', function( evt ) {
+    var openedMenu = document.querySelector( '.navbar-toggler ' ),
+        targetElement = evt.target;  // clicked element
 
     do {
         if ( targetElement == openedMenu ) {
-       
+            This is a click inside. Do nothing, just return.
             return;
         }
 
-       
+        Go up the DOM
         targetElement = targetElement.parentNode;
-    } while ( targetElement );
+    } while ( targetElement != openedMenu.click());
 
+    This is a click outside.
     openedMenu.click();
-} );
+} );*/
 
 
 //Scroll to top, when page refreshed
